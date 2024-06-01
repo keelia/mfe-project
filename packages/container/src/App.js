175d54ae171369f6caa6954c60020e47ccd324f9
Li Kang <li.kang@teletracnavman.com>
2024-06-02 10:04:10 +1200

    randomize production classname

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import Headder from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container-',
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Headder />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Headder />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
